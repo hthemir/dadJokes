10462f73ecedc9282829a9453e3d986405714b47
hugo <hthemir@hotmail.com>
2018-10-24 17:00:15 -0300

    add adview

--- app/src/main/java/com/example/rapisodo/mypoc/feature/dadjoke/DadJokeActivity.kt
+++ app/src/main/java/com/example/rapisodo/mypoc/feature/dadjoke/DadJokeActivity.kt
@@ -1,11 +1,15 @@
 package com.example.rapisodo.mypoc.feature.dadjoke
 
 import android.content.Intent
+import android.os.Bundle
 import android.widget.Toast
 import com.example.rapisodo.mypoc.R
 import com.example.rapisodo.mypoc.base.BaseDatabindingActivity
 import com.example.rapisodo.mypoc.databinding.ActivityJokeRelativeBinding
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.MobileAds
 import io.reactivex.disposables.CompositeDisposable
+import kotlinx.android.synthetic.main.activity_joke_relative.*
 
 open class DadJokeActivity : BaseDatabindingActivity<ActivityJokeRelativeBinding, DadJokeViewModel>() {
 
@@ -24,6 +28,8 @@ open class DadJokeActivity : BaseDatabindingActivity<ActivityJokeRelativeBinding
         compositeDisposable.add(viewModel.getRandomDadJoke().subscribe({}, { _ ->
             Toast.makeText(this, "batatao", Toast.LENGTH_SHORT)
         }))
+
+//        startAdmob()
     }
 
     override fun getViewModelClass(): Class<DadJokeViewModel> = DadJokeViewModel::class.java
@@ -35,6 +41,17 @@ open class DadJokeActivity : BaseDatabindingActivity<ActivityJokeRelativeBinding
             type = "text/plain"
         }
         startActivity(sendIntent)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startAdmob()
+    }
+
+    fun startAdmob() {
+        MobileAds.initialize(this, "ca-app-pub-5364425353095108~3821841778");
+        val adRequest = AdRequest.Builder().build()
+        dataBinding.adView.loadAd(adRequest)
     }
 
     override fun onDestroy() {
